92099d261729a8a971c1e05911c1d817f1d19f66
Bhagavati Braun <bhagavati.braun@gmail.com>
2020-07-29 12:05:03 +0300

    Simple Calculations Work!
    One-step calculations are now possible.

--- script.js
+++ script.js
@@ -21,23 +21,60 @@ function divide(array) {
 
 const operators = [add, subtract, multiply, divide];
 
-function operate(index, array){
+function operate(operator, array){
+	switch(operator) {
+		case '+':
+			return add(array);
+			break;
+		case 'subtract':
+			return subtract(array);
+			break;
+		case 'x':
+			return multiply(array);
+			break;
+		case 'divide':
+			return divide(array);
+			break;		
+	}
+	/*
 	if (index > 3) return null;
-	return operators[index](array);
+	else if (index == 'add') {
+		return add(array)
+	}
+	return operators[index](array);*/
 }
 		
-		let test = [2, 2, 3];
+		let testNum = [2, 2, 3, 7, 9];
+		let testOp = [add, subtract, multiply, divide];
 		let a = 3;
 		let b = 4;
-		console.log(add(test));
-		console.log(subtract(test));
-		console.log(multiply(test));
-		console.log(divide(test));
+		console.log(add(testNum));
+		console.log(subtract(testNum));
+		console.log(multiply(testNum));
+		console.log(divide(testNum));
+		let num2 = testNum.splice(2, 0, 4);
+		console.log(num2);
+		console.log(testNum.splice(2, 0, '4'));
+		//console.log(operate(testOp[1], testNum));
+		
+/* ====== trying to get xplice to work =======
+let months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+*/
 		
 /*-------------------------------------------------------*/		
 
 let numArray = [];
 let opArray = [];
+let result;
 
 
 //let displayValue = 678;
@@ -48,6 +85,7 @@ let opArray = [];
 // divs
 const container = document.querySelector('.container');
 const display = document.querySelector('#display');
+const inputDisplay = document.querySelector('#inputDisplay');
 //buttons
 const allBtns = document.querySelectorAll('button');
 const numBtn = document.querySelectorAll('.num');
@@ -64,36 +102,73 @@ allBtns.forEach((button) => {
 
 function getNum(element) {     
 	let el = element.textContent;
-	let elID = element.get
-	if (!isNaN(el)){
+	let elID = element.getAttribute('id');
+	if (!isNaN(el)) {
 		if (display.textContent == '0' || isNaN(display.textContent)) {
-			display.textContent = el;
+			display.textContent = elID;
 		}
 		else {
-			display.textContent += el;		
+			display.textContent += elID;		
 		}
 	}
 	else {
 		let num1 = display.textContent;
 		numArray.push(num1);
 		console.log(numArray);
-		opArray.push();
 		console.log(opArray);
 		
-		if (el == 'e'/*'='*/) {
-			operate(index, array);
+		if (elID == 'equals'/*'='*/) {
+			inputDisplay.textContent = `${numArray[0]} ${opArray[0]} ${numArray[1]}`
+			display.textContent = operate(opArray[0], numArray);
 		}
 		
 		else if (el == 'clear') {
 			display.textContent = 0;
+			inputDisplay.textContent = '';
+			numArray.length = 0;
+			opArray.length = 0;
 		}
 		else {
-			
+			opArray.push(el);
+			inputDisplay.textContent = num1 + ' ' + el;
 			display.textContent = el;
 		}
 	}	
 };
 
+
+//////I'M GETTING AHEAD OF MYSELF
+/////MAKE A BASIC CALC THAT CAN DO OPERATIONS IN ORDER OF ENTERING FIRST
+
+
+//running the calculations
+//====how to determine if the operator is
+//let multiply =. opArray.find(function(operator) {
+	//opArray === 'multiply';
+
+
+/*for (let i = 0, i < opArray.length; i++) {
+	let calcArray = [numArray[i], numArray[i+1]];
+	let toDoArray = [];
+	if (opArray[i] == 'subtract' || opArray[i] == 'add') {
+		i++;
+	}	
+	else {	
+		if (opArray[i] == 'multiply') {
+			numArray.slice(i, i+1).push
+			multiply(calcArray);
+			opArray = opArray.slice(i);
+		else if (opArray[i] == 'divide') divide(calcArray;)
+
+	}
+		else if (opArray[i] == 'subtract') {
+		subtract(calcArray);
+	}	
+	else if (opArray[i] == 'add') {
+		add(calcArray);
+	}
+}
+*/
 //console.log(displayValue);		
 //display.textContent = displayValue;
 
